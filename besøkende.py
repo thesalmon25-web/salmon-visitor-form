d52aaa276c25b79397864b841375b6acd8beb912
Shreeramghimire <ghimireshreeram25@gmail.com>
2025-07-03 13:27:04 +0200

    Updated naming from Lakssenteret to The Salmon Kunnskapssenteret

--- besøkende.py
+++ besøkende.py
@@ -45,7 +45,7 @@ translations = {
         "no": "Nei",
         "submit": "Send inn",
         "thanks": "✅ Takk for ditt svar!",
-        "welcome": "### 🎉 Velkommen til Laksesenteret!",
+        "welcome": "### 🎉 Velkommen til The Salmon Kunnskapssenteret!",
         "enjoy": "Vi gleder oss til å ta deg med på en guidet tur i senteret. Nyt opplevelsen!",
         "refresh": "🔄 Et nytt skjema vises om 5 sekunder..."
     }
